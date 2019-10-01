af7a4265faa8632d28fa4f7b575d6c6af6b7e016
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-10-01 17:20:46 +0000

    Automatically backed up by Learn

--- spec/sinatra_basic_views_lab_spec.rb
+++ spec/sinatra_basic_views_lab_spec.rb
@@ -7,25 +7,25 @@ describe 'App' do
       get '/'
     end
     
-    xit "returns a 200 status code" do
+    it "returns a 200 status code" do
       expect(last_response.status).to eq(200)
     end
 
 
-    xit "renders index.erb" do
+    it "renders index.erb" do
       expect(last_response.body).to eq(File.read("views/index.erb"))
     end
 
 
-    xit "displays the name of the bowling alley in an h1 tag" do
+    it "displays the name of the bowling alley in an h1 tag" do
       expect(last_response.body).to include("<h1>Buster's Best Bowling Alley</h1>")
     end
 
-    xit "displays the bowling alley tagline in an h2" do
+    it "displays the bowling alley tagline in an h2" do
       expect(last_response.body).to include("<h2>Bet You Can't Bowl A Strike Every Time</h2>")
     end
 
-    xit "displays the bowling alley address in a p tag" do
+    it "displays the bowling alley address in a p tag" do
       expect(last_response.body).to include("<p>97 Bowling Alley Lane, Boulder CO </p>")
     end
 
